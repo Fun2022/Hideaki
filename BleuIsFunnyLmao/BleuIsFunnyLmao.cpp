7de9a81735d5481595ba7d8900d60cae34049303
Hideaki <53636771+HideakiAtsuyo@users.noreply.github.com>
2022-02-18 07:32:58 -0800

    Update BleuIsFunnyLmao.cpp
    Expect an update before tomorrow(French time)

--- BleuIsFunnyLmao/BleuIsFunnyLmao.cpp
+++ BleuIsFunnyLmao/BleuIsFunnyLmao.cpp
@@ -3,17 +3,17 @@
 
 int main()
 {
-    cpr::Response r = cpr::Post(cpr::Url{ "https://tremendous-ivy-dogsled.glitch.me/api/register" },
+    cpr::Response r = cpr::Post(cpr::Url{ "https://blue-bypassme.herokuapp.com/api/register" },
         cpr::Header{
             { "Content-Type", "application/json" }
         },
         cpr::UserAgent("Hideaki Atsuyo"),
-        cpr::Body("{\"username\": \"test\", \"email\": \"test@example.com\", \"password\": \"test\"}"));
+        cpr::Body("{\"username\": \"Hideaki\", \"email\": \"testg@example.com\", \"password\": \"Password123.@\", \"confirmpassword\": \"Password123.@\"}"));
     if (r.text.find("ACCOUNT_CREATED") != std::string::npos) {
-        std::cout << "Success !" << std::endl;
+        std::cout << "Success !" << std::endl << r.text << std::endl;
     }
     else {
-        std::cout << "Failed !" << std::endl;
+        std::cout << "Failed !" << std::endl << r.text << std::endl;
     }
     system("pause");
     return 0;
